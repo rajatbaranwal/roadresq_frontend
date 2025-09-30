d5525093a039b7a66e866b70382c384bda8c7311
Rajat Baranwal <rajat123baranwal@gmail.com>
2025-09-30 22:26:27 +0530

    Update API baseURL to deployed backend

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5050/api',
+  baseURL: 'https://roadresq-backkend.onrender.com',
   withCredentials: true, // optional if not using cookies
 });
 
